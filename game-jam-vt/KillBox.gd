744f19a5963808cd4327125a0ffb8f32011f4782
tbongiorno <trbongiorno@gmail.com>
2026-03-28 15:17:52 -0400

    level 2 stuff

--- game-jam-vt/KillBox.gd
+++ game-jam-vt/KillBox.gd
@@ -1,15 +1,14 @@
 extends Area3D
 
 
-func _on_body_entered(body: Node3D) -> void:
-	print("hi")
+func _on_body_entered(body: Node3D) -> void:			
 	$"../Basic FPS Player".dashCards = 0
 	$"../Basic FPS Player".jumpCards = 0
 	if ($"../Basic FPS Player/Control/CardContainer".get_child_count() != 0):
 		for i in range(0, $"../Basic FPS Player/Control/CardContainer".get_child_count()):
 			$"../Basic FPS Player/Control/CardContainer".get_child(i).queue_free()
-			
-	if body.name == "Basic FPS Player":
+	
+	if body.name == "Basic FPS Player":	
 		var deathTransition = body.get_node("DeathTransition")
 		deathTransition.visible = true
 		var tween = get_tree().create_tween()
--- game-jam-vt/SwitchScreens.gd
+++ game-jam-vt/SwitchScreens.gd
@@ -8,4 +8,9 @@ func _on_body_entered(body: Node3D) -> void:
 		var tween = get_tree().create_tween()
 		tween.tween_property(deathTransition.get_node("ColorRect").material, "shader_parameter/height", 0.67, 0.5)
 		await get_tree().create_timer(0.75).timeout
-		get_tree().change_scene_to_file("res://level2.tscn")
+		
+		var level = $"../"
+		if level.name == "Tutorial":
+			get_tree().change_scene_to_file("res://level2.tscn")
+		elif level.name == "level2":
+			get_tree().change_scene_to_file("res://level3.tscn")
