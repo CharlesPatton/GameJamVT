1674fc64e4a6a352aba5e72ad2ad7b1a9d106029
tbongiorno <trbongiorno@gmail.com>
2026-03-29 15:54:39 -0400

    lotta stuff

--- game-jam-vt/KillBox.gd
+++ game-jam-vt/KillBox.gd
@@ -5,13 +5,13 @@ func _on_body_entered(body: Node3D) -> void:
 	
 	
 	if body.name == "Basic FPS Player":	
-		#$death_sound_effect.play() #DELETE/COMMENT OUT FOR SOUND
+		$death_sound_effect.play() #DELETE/COMMENT OUT FOR SOUND
 		$"../Basic FPS Player".dashCards = 0
 		$"../Basic FPS Player".jumpCards = 0
 		if ($"../Basic FPS Player/Control/CardContainer".get_child_count() != 0):
 			for i in range(0, $"../Basic FPS Player/Control/CardContainer".get_child_count()):
 				$"../Basic FPS Player/Control/CardContainer".get_child(i).queue_free()
-		for i in range(3, self.get_child_count()):
+		for i in range(4, self.get_child_count()):
 			var node = self.get_child(i)
 			if node.name.contains("log"):
 				node = node.get_child(2)
