99a1ac5e60674099eadb794f984049c01a35f5b5
LoreAccurateLogan <loganjoyce@vt.edu>
2026-03-29 01:16:29 -0400

    Level 4 update

---
+++ game-jam-vt/models/planet.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.global_position = get_tree().current_scene.get_node("Basic FPS Player").global_position
+	global_position.x -= 100
+	global_position.y += 0
+	global_position.z -= 100
