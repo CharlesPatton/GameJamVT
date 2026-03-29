66cf3fd64fa99d40a306568f93a66c2bd766823b
Charpatt <cpatt1234@icloud.com>
2026-03-29 16:20:29 -0400

    stuff

--- game-jam-vt/intrsuctions_1.gd
+++ game-jam-vt/intrsuctions_1.gd
@@ -2,7 +2,6 @@ extends Area3D
 
 
 func _on_body_entered(body: Node3D) -> void:
-		
 	if body.name == "Basic FPS Player":
 		var tween = get_tree().create_tween()
 		tween.tween_property($Control/Label, "visible_ratio", 1, 0.5)
